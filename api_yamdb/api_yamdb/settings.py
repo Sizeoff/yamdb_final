53d9f5a59c6e498179e5798da8bfd3ef5c434df1
Владислав Сизов <Vladsizov96@yandex.ru>
2022-09-25 17:01:05 +0300

    after review

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 from dotenv import load_dotenv
 
 load_dotenv('../infra/.env')
-
+    
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY')
